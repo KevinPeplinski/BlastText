5659637b663e995d0a6e3d51da13db979d658038
Kevin Peplinski <k.peplinski@gmx.de>
2021-02-22 11:25:48 +0100

    Add Tests

--- Tests/BlastTextTests/BlastTextTests.swift
+++ Tests/BlastTextTests/BlastTextTests.swift
@@ -1,15 +1,92 @@
 import XCTest
+import SwiftUI
 @testable import BlastText
 
 final class BlastTextTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BlastText().text, "Hello, World!")
+    
+    let stringToBlast = "Lorem ipsum dolor sit amet, voluptua. At et ea rebum."
+    
+    func testBlastTextWithAllDelimiter() {
+        let blastSegmentsValues = BlastText(verbatim: stringToBlast, delimiter: .all).blastSegments.map {
+            $0.value
+        }
+        
+        let expectedResult: [String] = ["L", "o", "r", "e", "m", " ", "i",
+                                        "p", "s", "u", "m", " ", "d", "o",
+                                        "l", "o", "r", " ", "s", "i", "t",
+                                        " ", "a", "m", "e", "t", ",", " ",
+                                        "v", "o", "l", "u", "p", "t", "u",
+                                        "a", ".", " ", "A", "t", " ", "e",
+                                        "t", " ", "e", "a", " ", "r", "e",
+                                        "b", "u", "m", "."]
+        
+        XCTAssertEqual(blastSegmentsValues, expectedResult)
+    }
+    
+    func testBlastTextWithCharacterDelimiter() {
+        let blastSegmentsValues = BlastText(verbatim: stringToBlast, delimiter: .character).blastSegments.map {
+            $0.value
+        }
+        
+        let expectedResult: [String] = ["L", "o", "r", "e", "m ", "i", "p",
+                                        "s", "u", "m ", "d", "o","l", "o",
+                                        "r ", "s", "i", "t ", "a", "m", "e",
+                                        "t", ", ", "v", "o", "l", "u", "p",
+                                        "t", "u", "a", ". ", "A", "t ", "e",
+                                        "t ", "e", "a ", "r", "e", "b", "u",
+                                        "m", "."]
+        
+        XCTAssertEqual(blastSegmentsValues, expectedResult)
+    }
+    
+    func testBlastTextWithWordDelimiter() {
+        let blastSegmentsValues = BlastText(verbatim: stringToBlast, delimiter: .word).blastSegments.map {
+            $0.value
+        }
+        
+        let expectedResult: [String] = ["Lorem ", "ipsum ", "dolor ",
+                                        "sit ", "amet, ", "voluptua. ",
+                                        "At ", "et ", "ea ", "rebum."]
+        
+        XCTAssertEqual(blastSegmentsValues, expectedResult)
+    }
+    
+    func testBlastTextWithSentenceDelimiter() {
+        let blastSegmentsValues = BlastText(verbatim: stringToBlast, delimiter: .sentence).blastSegments.map {
+            $0.value
+        }
+        
+        let expectedResult: [String] = ["Lorem ipsum dolor sit amet, voluptua. ",
+                                        "At et ea rebum."]
+        
+        XCTAssertEqual(blastSegmentsValues, expectedResult)
+    }
+    
+    func testBlastTextWithCustomDelimiter() {
+        let blastSegmentsValues = BlastText(verbatim: stringToBlast, delimiter: .custom(regex: "(um)")).blastSegments.map {
+            $0.value
+        }
+        
+        let expectedResult: [String] = ["um", "um"]
+        
+        XCTAssertEqual(blastSegmentsValues, expectedResult)
+    }
+    
+    func testBlastTextWithLocalizedInitializerButNoMatchInLocalizationFileOrNoLocalizationFileProvided() {
+        
+        let key = LocalizedStringKey("Localized_Key_But_No_Match_In_Localization_File_Or_No_Localization_File_Provided")
+        
+        let test = BlastText(key, delimiter: .word)
+        
+        XCTAssertTrue(test.blastSegments[0].value == key.stringValue())
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testBlastTextWithAllDelimiter", testBlastTextWithAllDelimiter),
+        ("testBlastTextWithCharacterDelimiter", testBlastTextWithCharacterDelimiter),
+        ("testBlastTextWithWordDelimiter", testBlastTextWithWordDelimiter),
+        ("testBlastTextWithSentenceDelimiter", testBlastTextWithSentenceDelimiter),
+        ("testBlastTextWithCustomDelimiter", testBlastTextWithCustomDelimiter),
+        ("testBlastTextWithLocalizedInitializerButNoMatchInLocalizationFileOrNoLocalizationFileProvided", testBlastTextWithLocalizedInitializerButNoMatchInLocalizationFileOrNoLocalizationFileProvided)
     ]
 }
